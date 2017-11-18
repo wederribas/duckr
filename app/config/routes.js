4b3f309485425348443cb073df9e6023e0e63d44
Weder Ribas <wederribas@gmail.com>
2017-11-18 15:35:35 -0200

    Create project main structure and files

---
+++ app/config/routes.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+} from 'react-router-dom';
+import { MainContainer } from '../containers';
+
+const routes = (
+  <Router>
+    <Route exact path="/" component={MainContainer} />
+  </Router>
+);
+
+export default routes;
---
+++ app/containers/Main/MainContainer.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const MainContainer = () => <p>Hello World!</p>;
+
+export default MainContainer;

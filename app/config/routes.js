c86fbde3074f386ce6449d37d27980770b69b37a
Weder Ribas <wederribas@gmail.com>
2017-11-18 17:22:17 -0200

    Change import path based on webpack resolve alias

--- app/config/routes.js
+++ app/config/routes.js
@@ -1,9 +1,6 @@
 import React from 'react'
-import {
-  BrowserRouter as Router,
-  Route,
-} from 'react-router-dom'
-import { MainContainer } from '../containers'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { MainContainer } from 'containers'
 
 const routes = (
   <Router>

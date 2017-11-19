310e693ac711fc7f480a7da3aeefeace76d160e3
Weder Ribas <wederribas@gmail.com>
2017-11-19 11:45:04 -0200

    Create facebook auth button component and its styles

--- app/components/Authenticate/Authenticate.js
+++ app/components/Authenticate/Authenticate.js
@@ -1,5 +1,11 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import {
+  centeredContainer,
+  largeHeader,
+  errorMsg,
+} from 'sharedStyles/styles.css'
+import { FacebookAuthButton } from 'components'
 
 Authenticate.propTypes = {
   error: PropTypes.string.isRequired,
@@ -9,10 +15,10 @@ Authenticate.propTypes = {
 
 export default function Authenticate ({ error, isFetching, onAuth }) {
   return (
-    <div>
-      <h1>{'Authenticate'}</h1>
-      <p>{'Facebook auth button'}</p>
-      {error ? <p>{'error'}</p> : null}
+    <div className={centeredContainer}>
+      <h1 className={largeHeader}>{'Authenticate'}</h1>
+      <FacebookAuthButton isFetching={isFetching} onAuth={onAuth} />
+      {error ? <p className={errorMsg}>{error}</p> : null}
     </div>
   )
 }

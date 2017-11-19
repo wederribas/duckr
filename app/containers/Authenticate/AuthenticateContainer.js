2890ec73a5d586aa98edf1b4c5de5ae8ac73f1ba
Weder Ribas <wederribas@gmail.com>
2017-11-19 16:15:22 -0200

    Add redux store and connect auth component to redux

--- app/containers/Authenticate/AuthenticateContainer.js
+++ app/containers/Authenticate/AuthenticateContainer.js
@@ -1,14 +1,50 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
 import { Authenticate } from 'components'
 import auth from 'helpers/auth'
+import * as userActionCreators from 'reduxConfig/modules/users'
 
-export default class AuthenticateContainer extends Component {
-  handleAuth () {
-    auth().then(user => {
-      console.log('Authed User', user)
-    })
+class AuthenticateContainer extends Component {
+  static propTypes = {
+    isFetching: PropTypes.bool.isRequired,
+    error: PropTypes.string.isRequired,
+    authUser: PropTypes.func.isRequired,
+    fetchingUser: PropTypes.func.isRequired,
+    fetchingUserFailure: PropTypes.func.isRequired,
+    fetchingUserSuccess: PropTypes.func.isRequired,
+  }
+  handleAuth = () => {
+    this.props.fetchingUser()
+    auth()
+      .then(user => {
+        this.props.fetchingUserSuccess(user.uid, user, Date.now())
+        this.props.authUser(user.uid)
+      })
+      .catch(error => this.props.fetchingUserFailure(error))
   }
   render () {
-    return <Authenticate isFetching={false} error='' onAuth={this.handleAuth} />
+    return (
+      <Authenticate
+        isFetching={this.props.isFetching}
+        error={this.props.error}
+        onAuth={this.handleAuth}/>
+    )
   }
 }
+
+function mapStateToProps (state) {
+  return {
+    isFetching: state.isFetching,
+    error: state.error,
+  }
+}
+
+function mapDispatchToProps (dispatch) {
+  return bindActionCreators(userActionCreators, dispatch)
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(
+  AuthenticateContainer
+)
--- app/index.js
+++ app/index.js
@@ -1,7 +1,13 @@
+import React from 'react'
 import ReactDOM from 'react-dom'
-import routes from './config/routes'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import routes from 'config/routes'
+import users from 'reduxConfig/modules/users'
+
+const store = createStore(users)
 
 ReactDOM.render(
-  routes,
+  <Provider store={store}>{routes}</Provider>,
   document.getElementById('app')
 )

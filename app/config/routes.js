389b7c53b2652c865461265c57e150e9a5aa9c23
Weder Ribas <wederribas@gmail.com>
2017-11-18 22:32:26 -0200

    Refactor react router to be v4 compliant

--- app/config/routes.js
+++ app/config/routes.js
@@ -1,11 +1,15 @@
 import React from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
-import { MainContainer } from 'containers'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { MainContainer, HomeContainer } from 'containers'
 
 const routes = (
-  <Router>
-    <Route exact path='/' component={MainContainer} />
-  </Router>
+  <MainContainer>
+    <Router>
+      <Switch>
+        <Route exact path='/' component={HomeContainer} />
+      </Switch>
+    </Router>
+  </MainContainer>
 )
 
 export default routes

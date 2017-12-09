89b7c470d7f11cd7cf8a305ab54e9f10bc5cb671
Weder Ribas <wederribas@gmail.com>
2017-12-09 11:31:39 -0200

    Create usersLikes and likeCount redux reducers

---
+++ app/reduxConfig/modules/usersLikes.js
@@ -0,0 +1,123 @@
+import {
+  fetchUsersLikes,
+  saveToUserLikes,
+  deleteFromUsersLikes,
+  incrementNumberOfLikes,
+  decrementNumberOfLIkes,
+} from 'helpers/api'
+
+export const ADD_LIKE = 'ADD_LIKE'
+export const REMOVE_LIKE = 'REMOVE_LIKE'
+const FETCHING_LIKES = 'FETCHING_LIKES'
+const FETCHING_LIKES_ERROR = 'FETCHING_LIKES_ERROR'
+const FETCHING_LIKES_SUCCESS = 'FETCHING_LIKES_SUCCESS'
+
+function fetchingLikes () {
+  return {
+    type: FETCHING_LIKES,
+  }
+}
+
+function fetchingLikesError () {
+  return {
+    type: FETCHING_LIKES_ERROR,
+    error: 'Error fetching likes',
+  }
+}
+
+function fetchingLikesSuccess (uid, duckIds, lastUpdated) {
+  return {
+    type: FETCHING_LIKES_SUCCESS,
+    uid,
+    duckIds,
+    lastUpdated,
+  }
+}
+
+function addLike (duckId) {
+  return {
+    type: ADD_LIKE,
+    duckId,
+  }
+}
+
+function removeLike (duckId) {
+  return {
+    type: REMOVE_LIKE,
+    duckId,
+  }
+}
+
+export function addAndHandleLike (duckId, e) {
+  e.stopPropagation()
+  return function (dispatch, getState) {
+    dispatch(addLike(duckId))
+
+    const uid = getState().users.authedId
+    Promise.all([
+      saveToUserLikes(uid, duckId),
+      incrementNumberOfLikes(duckId),
+    ]).catch(error => {
+      console.warn(error)
+      dispatch(removeLike(duckId))
+    })
+  }
+}
+
+export function handleDeleteLike (duckId, e) {
+  e.stopPropagation()
+  return function (dispatch, getState) {
+    dispatch(removeLike(duckId))
+
+    const uid = getState().users.authedId
+    Promise.all([
+      deleteFromUsersLikes(uid, duckId),
+      decrementNumberOfLIkes(duckId),
+    ]).catch(error => {
+      console.warn(error)
+      dispatch(addLike(duckId))
+    })
+  }
+}
+
+const initialState = {
+  isFetching: false,
+  error: '',
+}
+
+export default function usersLikes (state = initialState, action) {
+  switch (action.type) {
+    case FETCHING_LIKES:
+      return {
+        ...state,
+        isFetching: true,
+      }
+    case FETCHING_LIKES_ERROR:
+      return {
+        ...state,
+        isFetching: false,
+        error: action.error,
+      }
+    case FETCHING_LIKES_SUCCESS:
+      return {
+        ...state,
+        ...action.likes,
+        isFetching: false,
+        error: '',
+      }
+    case ADD_LIKE:
+      return {
+        ...state,
+        [action.duckId]: true,
+      }
+    case REMOVE_LIKE:
+      return Object.keys(state)
+        .filter(duckId => action.duckId !== duckId)
+        .reduce((prev, current) => {
+          prev[current] = state[current]
+          return prev
+        }, {})
+    default:
+      return state
+  }
+}
